cff0c48ebcbbbd674b351187c05911d2d8df620a
SamuelOrtizT <saortizt@unal.edu.co>
2023-10-08 23:00:52 -0500

    Intento 1

---
+++ src/main/java/comunicacion/Alfabeto.java
@@ -0,0 +1,36 @@
+package comunicacion;
+
+public class Alfabeto extends Pictograma{
+    private String[] letras;
+    private String interpretacion;
+    public Alfabeto(String origen, String[] letras, String interpretacion) {
+        super(origen);
+        this.letras = letras;
+        this.interpretacion = interpretacion;
+    }
+    public String[] getLetras() {
+        return this.letras;
+    }
+    public void setLetras(String[] letras) {
+        this.letras = letras;
+    }
+    public String getInterpretacion() {
+        return this.interpretacion;
+    }
+    public void setInterpretacion(String interpretacion) {
+        this.interpretacion = interpretacion;
+    }
+    public int cantidadLetras() {
+        return this.letras.length;
+    }
+    public String interpretacion() {
+        return this.interpretacion;
+    }
+    public String toString() {
+        String r = "";
+        for (int i = 0; i < this.cantidadLetras(); i++) {
+            r += letras[i]+", ";
+        }
+        return r;
+    }
+}
